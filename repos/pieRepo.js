625689318fc8784d7157a381013318f971523753
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2021-10-12 13:37:34 +0600

    search for data

--- repos/pieRepo.js
+++ repos/pieRepo.js
@@ -21,6 +21,28 @@ let pieRepo = {
         resolve(pie);
       }
     });
+  },
+  search: function (searchObject, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        let pies = JSON.parse(data);
+        // perform search
+        if (searchObject) {
+          // Example search Object
+          // let searchObject = {
+          //   "id":1,
+          //   "name": 'A'
+          // };
+          pies = pies.filter(
+            p => (searchObject.id ? p.id == searchObject.id : true) &&
+              (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0 : true));
+        }
+
+        resolve(pies);
+      }
+    });
   }
 };
 
